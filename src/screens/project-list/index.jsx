2ab326e60d8f97701c88c2571e1d3a9cb1f245fb
wangxinyue <1977795940@qq.com>
2021-03-20 20:53:12 +0800

    添加usemount和useDebounce两个Hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {SearchPanel} from "./search-panel";
 import {List} from "./list";
-import {cleanObject} from "../../utils";
+import {cleanObject,useMount,useDebounce} from "../../utils";
 import {useEffect, useState} from "react";
 import qs from 'qs';
 
@@ -13,25 +13,27 @@ export const ProjectListScreen = () =>{
     personId:''
   });
 
+  const debouncedParam = useDebounce(param,2000)
+
   const [list,setList] = useState([]);
 
   const [users,setUsers] = useState([])
 
   useEffect(()=>{
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response =>{
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response =>{
       if(response.ok){
         setList(await response.json())
       }
     })
-  },[param])
+  },[debouncedParam])
 
-  useEffect(()=>{
+  useMount(()=>{
     fetch(`${apiUrl}/users`).then(async response =>{
       if(response.ok){
         setUsers(await response.json())
       }
     })
-  },[])
+  });
 
   return <div>
       <SearchPanel users={users} param={param} setParam={setParam}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,6 @@
-export const isFalsy = (value) => value === 0 ? false : !value
+import {useEffect,useState} from "react";
+
+export const isFalsy = (value) => value === 0 ? false : !value;
 
 //在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object) =>{
@@ -10,6 +12,25 @@ export const cleanObject = (object) =>{
     if(isFalsy(value)){
       delete result[key]
     }
-  })
+  });
   return result
-}
+};
+
+export const useMount = (callback)=>{
+  useEffect(()=>{
+    callback()
+  },[])
+};
+
+export const useDebounce = (value,delay) =>{
+  const [debounceValue,setDebounceValue] = useState(value)
+
+  useEffect(()=>{
+  //  每次在value变化以后， 设置一个定时器
+    const timeout = setTimeout(()=>setDebounceValue(value),delay);
+  //  每次在上一个useEffect处理完以后再运行
+    return ()=>clearTimeout(timeout)
+  },[value,delay]);
+
+  return debounceValue
+};
